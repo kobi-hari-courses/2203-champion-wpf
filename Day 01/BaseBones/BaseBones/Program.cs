a152302f786c6e8ab4dc942e01d794a3a6018070
Kobi Hari <kobi2294@yahoo.com>
2022-03-03 11:14:17 +0200

    Day 1 Break

---
+++ Day 01/BaseBones/BaseBones/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace BaseBones
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var win = new Window();
+            win.Title = "Hello World";
+
+            var sp = new StackPanel();
+
+            var ellipse = new Ellipse()
+            {
+                Width = 100,
+                Height = 100,
+                Fill = Brushes.Red
+            };
+
+            sp.Children.Add(ellipse);
+
+            var tb = new TextBlock()
+            {
+                Text = "Hello",
+                Foreground = Brushes.Blue,
+                FontSize = 16.0
+            };
+
+            sp.Children.Add(tb);
+
+            win.Content = sp;
+
+            var app = new Application();
+
+            app.Run(win);
+
+        }
+    }
+}
---
+++ Day 01/FunWithXaml/FunWithXaml/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace FunWithXaml
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public double Age { get; set; }
+
+        public Color FavoriteColor { get; set; }
+
+        public Person()
+        {
+
+        }
+    }
+}
