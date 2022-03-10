386851fe0055c6e384aad0cf601f5214898c094a
Kobi Hari <kobi2294@yahoo.com>
2022-03-10 16:47:38 +0200

    resources

---
+++ Day 02/Ex1Solution/Ex1Solution/Character.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1Solution
+{
+    public class Character
+    {
+        public string FontSize { get; set; }
+        public string FontName { get; set; }
+        public string Text { get; set; }
+    }
+}
