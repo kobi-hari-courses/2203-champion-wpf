d0ff44a4ae0a6b8736589451c84e8d7f609c63f5
Kobi Hari <kobi2294@yahoo.com>
2022-05-15 13:49:21 +0300

    tpl example completed

--- Day 05/FunWithTpl/FunWithTpl/MainWindow.xaml.cs
+++ Day 05/FunWithTpl/FunWithTpl/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +22,33 @@ namespace FunWithTpl
     /// </summary>
     public partial class MainWindow : Window
     {
+        private CancellationTokenSource? _cts = null;
+        private TaskCompletionSource<(int from, int to)>? _tcs = null;
+
+        private Task<(int from, int to)> GetRangeFromUser(CancellationToken? ct = null)
+        {
+            _tcs = new TaskCompletionSource<(int from, int to)>();
+            using (ct?.Register(() =>
+            {
+                _tcs.SetCanceled();
+                _tcs = null;
+                rangePanel.Visibility = Visibility.Collapsed;
+            }))
+
+            rangePanel.Visibility = Visibility.Visible;
+            return _tcs.Task;
+        }
+
+        private void btnSetRange_Click(object sender, RoutedEventArgs e)
+        {
+            var from = int.Parse(txtFrom.Text);
+            var @to = int.Parse(txtTo.Text);
+
+            _tcs?.SetResult((from, to));
+            _tcs = null;
+            rangePanel.Visibility = Visibility.Collapsed;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -51,19 +80,70 @@ namespace FunWithTpl
 
         private async void btnStart_Click(object sender, RoutedEventArgs e)
         {
-            btnStart.IsEnabled = false;
-            txt.Text = "Please wait";
-            progress.IsIndeterminate = true;
-
-            var task = PrimesCalculator
-                    .GetAllPrimesAsync(2, 350000);
-
-            listBox.ItemsSource = await task;
-
-            progress.IsIndeterminate = false;
-            txt.Text = "Completed";
-            btnStart.IsEnabled = true;
+            Debug.WriteLine("A");
+            await runCalc();
+            Debug.WriteLine("B");
 
         }
+
+        private async Task<int> runCalc()
+        {
+            var watch = new Stopwatch();
+
+            try
+            {
+                Debug.WriteLine("1");
+                btnStart.IsEnabled = false;
+                txt.Text = "Please wait";
+                _cts = new CancellationTokenSource();
+
+                var range = await GetRangeFromUser(_cts.Token);
+
+                watch.Start();
+                var progressReport = new Progress<double>(val => progress.Value = val);
+
+                //progress.IsIndeterminate = true;
+
+                var task = PrimesCalculator
+                        .GetAllPrimesAsync(range.from, range.to, _cts.Token, progressReport);
+                var res = await task;
+
+                //var tasks = Enumerable.Range(0, 5)
+                //    .Select(i => PrimesCalculator.GetAllPrimesAsync(70000 * i, 70000 * (i + 1) - 1));
+                //var task = Task.WhenAll(tasks);
+
+                //var results = await task;
+
+                //var res = results.SelectMany(i => i).ToList();
+
+
+                Debug.WriteLine("2");
+                listBox.ItemsSource = res;
+                Debug.WriteLine("3");
+
+                txt.Text = "Completed";
+                Debug.WriteLine("4");
+                _cts = null;
+
+            } catch (OperationCanceledException)
+            {
+                txt.Text = "Operation Cancelled";
+            }
+            finally
+            {
+                //progress.IsIndeterminate = false;
+                btnStart.IsEnabled = true;
+                watch.Stop();
+                Debug.WriteLine($"Operation took {watch.Elapsed} to complete");
+
+            }
+            return 2 + 2;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            _cts?.Cancel();
+        }
+
     }
 }
--- Day 05/FunWithTpl/FunWithTpl/PrimesCalculator.cs
+++ Day 05/FunWithTpl/FunWithTpl/PrimesCalculator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FunWithTpl
@@ -19,19 +20,46 @@ namespace FunWithTpl
         }         
 
 
-        public static IEnumerable<int> GetAllPrimes(int start, int finish)
+        public static IEnumerable<int> GetAllPrimes(int start, int finish, CancellationToken ct, IProgress<double>? progress)
         {
+            var total = finish - start;
+
             for (int i = start; i <= finish; i++)
             {
+                var soFar = i - start;
+                if (soFar % 1000 == 0)
+                {
+                    progress?.Report((double)soFar / total);
+                }
+
+                //if (ct.IsCancellationRequested) throw new OperationCanceledException();
+                ct.ThrowIfCancellationRequested();
+
                 if (_isPrime(i)) yield return i;
             }
 
+            progress?.Report(1);
+
         }
 
-        public static Task<List<int>> GetAllPrimesAsync(int start, int finish)
+        public static IEnumerable<int> GetAllPrimesParallel(int start, int finish, CancellationToken ct)
         {
+            return Enumerable.Range(start, finish)
+                .AsParallel()
+                .AsOrdered()                
+                .WithCancellation(ct)
+                .Where(i => _isPrime(i));
+        }
+
+
+
+        public static Task<List<int>> GetAllPrimesAsync(int start, int finish,
+                CancellationToken? ct = null, IProgress<double>? progress = null)
+        {
+            var token = ct.HasValue ? ct.Value : CancellationToken.None;
+
             return Task.Factory.StartNew(() => 
-                 GetAllPrimes(start, finish).ToList()
+                 GetAllPrimes(start, finish, token, progress).ToList()
                  );
         }
 
